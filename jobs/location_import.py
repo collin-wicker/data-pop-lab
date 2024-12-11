0f66ef809366ad9c20e80cf79388a2fb32b4302a
Collin Wicker <collin.wicker@networktocode.com>
2024-12-11 18:23:49 -0500

    Update location_import

--- jobs/location_import.py
+++ jobs/location_import.py
@@ -1,8 +1,11 @@
 import csv
 import logging
-from io import StringIO
+from io import TextIOWrapper
 
 from nautobot.apps.jobs import FileVar, Job, register_jobs
+from nautobot.dcim.models import Location, LocationType
+from nautobot.extras.models import Status
+from .state_abbreviations import STATE_ABBREVIATIONS
 
 class LocationImportJob(Job):
     """This job imports locations from a CSV file or CSV text input."""
@@ -17,21 +20,25 @@ class LocationImportJob(Job):
         name = "Location Import Job"
         description = "This job imports locations from a CSV file or CSV text input."
     
+    def _find_state(self, state_abbr):
+        state = STATE_ABBREVIATIONS.get(state_abbr)
+        return state
+
     def _process_csv_file(self, csv_file):
         """Convert CSV data into a dictionary containing Nautobot objects."""
         self.logger.info("Decoding CSV file...")
         with open(csv_file, "rb") as file:
+            text_file = TextIOWrapper(file, encoding="utf-8")
             csv_reader = csv.DictReader(file)
+            state_location_type = LocationType.objects.get(name="State")
+            city_location_type = LocationType.objects.get(name="City")
+            active_status_object = Status.objects.get(name="Active")
+
             for row in csv_reader:
-                name = row.get("name")
-                print(name)
-                city = row.get("city")
-                print(city)
-                state = row.get("state")
-                print(state)
-        # text_file = csv_file.read().decode("utf-8")
-        # for row in decoded_csv_file:
-        #     print(row)
+                print(row["name"])
+                print(row["city"])
+                print(row["state"])
+
 
     
     def run(self, csv_file):
