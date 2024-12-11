9d4f942270e05878a743f871a0d6524ff2a13753
Collin Wicker <collin.wicker@networktocode.com>
2024-12-11 16:13:48 -0500

    Update location_import

--- jobs/location_import.py
+++ jobs/location_import.py
@@ -1,23 +1,35 @@
 import csv
+import logging
+from io import StringIO
 
 from nautobot.apps.jobs import FileVar, Job, register_jobs
 
 class LocationImportJob(Job):
     """This job imports locations from a CSV file or CSV text input."""
 
-    file_input = FileVar()
+    csv_file = FileVar(
+        label="CSV File",
+        required=True,
+        description="Provide the CSV file of locations to import."
+    )
 
     class Meta: 
         name = "Location Import Job"
         description = "This job imports locations from a CSV file or CSV text input."
     
-    def run(self, file_input):
+    def _process_csv_file(self, csv_file):
+        """Convert CSV data into a dictionary containing Nautobot objects."""
+        self.logger.info("Decoding CSV file...")
+        decoded_csv_file = csv_file.read().decode("utf-8")
+        print(decoded_csv_file)
+        # csv_reader = csv.DictReader(StringIO(decoded_csv_file))
+        # self.logger.info("Processing CSV data...")
+        # processing_failed = False
+        # processed_csv_data = {}
+
+    
+    def run(self, csv_file):
         """Do actions."""
-        file_input.form_field.to_python()
-        # Open the CSV file
-        # with open(file_input, mode='r') as file:
-        #     csv_reader = csv.DictReader(file)
-        #     for row in csv_reader:
-        #         print(row)
+       _process_csv_file(csv_file)
 
 register_jobs(LocationImportJob)
