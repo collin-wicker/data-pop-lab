0a7e6097124557d73fde53abbe892b57e6a4946f
Collin Wicker <collin.wicker@networktocode.com>
2024-12-11 14:40:55 -0500

    Modify job

--- jobs/location_import.py
+++ jobs/location_import.py
@@ -1,10 +1,7 @@
 from nautobot.apps.jobs import Job, register_jobs
-from nautobot.extras.jobs import FileVar
 
 class LocationImportJob(Job):
     """This job imports locations from a CSV file or CSV text input."""
-
-    file_input = FileVar()
 
     class Meta: 
         name = "Location Import Job"
